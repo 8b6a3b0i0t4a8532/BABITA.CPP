a044672da4c12726e8fc3ed57be770d3dadccbae
8b6a3b0i0t4a8532 <160332405+8b6a3b0i0t4a8532@users.noreply.github.com>
2024-10-26 13:30:28 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,15 @@
+// write a program to check the number is even or odd
+
+# include <iostream>
+using namespace std;
+int main()
+{
+  int num;
+cout<<"enter the number:";
+cin>>num;
+if (num % 2 == 0)
+    cout<<"number is even";
+else
+    cout<<"number is odd";
+return 0;
+}
